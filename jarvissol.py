be51bc5cffc6e5e4d2b724e3b2a8143a64d89487
cuemat <82495157+cuemat@users.noreply.github.com>
2021-04-15 06:02:27 +0530

    Update jarvissol.py

--- jarvissol.py
+++ jarvissol.py
@@ -1,7 +1,70 @@
-#What is an A.I. assistant? 
-#Can you give me some examples? 
-#Have you ever wondered how cool it would be to have your own A.I. assistant? 
-#What would you want to make your A.I. assistant do for you? 
-#Imagine how easier it would be to make the assistant solve math calculations? 
-#Let us create an Assistant who can solve the math problems for you.
-#To create a project, 
+import pyttsx3
+import speech_recognition as sr
+
+
+engine = pyttsx3.init()  # to call the initial function
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+
+def wishme():
+    speak("Hello student, Wellcome back")
+    speak("Jarvis at your service, please let me know how can I help you")
+
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening....")
+        audio = r.record(source, duration=8)
+        print(source)
+
+    try:
+        print("recognizing...")
+        query = r.recognize(audio)
+        print(f"you said {query}")
+    except Exception as e:
+        print(e)
+        speak("I didn't quite understand, say that again please!")
+        return "None"
+
+    return query
+
+
+def eval_binary_expr(op1, oper, op2):
+    op1,op2 = int(op1), int(op2)
+    print(op1,op2)
+    print(oper)
+    if oper == "+":
+        speak(f"The answer is {op1+op2}")
+    elif oper == "-":
+        speak(f"The answer is {op1-op2}")
+    elif oper =="x" or "X":
+        speak(f"The answer is {op1*op2}")
+    elif oper == "Divided" or "divided":
+        speak(f"The answer is {op1/op2}")
+
+
+
+if __name__ == "__main__":
+    wishme()
+    while True:
+        query = takecommand().lower()
+  
+
+        elif "solve" in query:
+            speak("Please tell me the math problem you want me to solve, please try saying 3 plus 3")
+            prob = takecommand()
+            print(f"you said {prob.split()}")
+            eval_binary_expr(*(prob.split()))
+
+
+        elif "offline" in query:
+            speak("I am glad to be of your service, take care")
+            quit()
+
+
