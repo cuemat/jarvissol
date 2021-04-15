1ece4973f2dc534b813a04260696a7349ff3a36b
cuemat <82495157+cuemat@users.noreply.github.com>
2021-04-15 05:58:34 +0530

    Create jarvissol.py

---
+++ jarvissol.py
@@ -0,0 +1,7 @@
+#What is an A.I. assistant? 
+#Can you give me some examples? 
+#Have you ever wondered how cool it would be to have your own A.I. assistant? 
+#What would you want to make your A.I. assistant do for you? 
+#Imagine how easier it would be to make the assistant solve math calculations? 
+#Let us create an Assistant who can solve the math problems for you.
+#To create a project, 
